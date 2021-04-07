47a13a09ecfdb4f3320094da80227d33b5b16404
Bar Levi Atias <barlevi.atias@gmail.com>
2021-04-07 19:05:42 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+console.log('Starting up');
+
+function init() {
+    renderProject()
+}
+
+function renderProject() {
+    var proj=getProject()
+    
+    var strHtmls=proj.map(function (proj) {
+        return `<div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-proj-id="${proj.id}" data-toggle="modal" href="#portfolioModal">
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">
+              <i class="fa fa-plus fa-3x"></i>
+            </div>
+          </div>
+          <img class="img-fluid" src="img/portfolio/${proj.id}.jpg" alt="">
+        </a>
+        <div class="portfolio-caption">
+          <h4>${proj.name}</h4>
+          <p class="text-muted">${proj.title}</p>
+        </div>`
+    })
+    console.log(strHtmls);
+    console.log($('.projs-container'));
+    $('.projs-container').html(strHtmls);
+    $('.portfolio-link').click(function(){
+        renderModal($(this).data('proj-id'))
+    })
+   
+
+}
+
+function renderModal(projId){
+    console.log(projId);
+    var currProj=getProjById(projId)
+    $('.portfolio-modal h2').text(currProj.name)
+    $('.portfolio-modal .item-intro').text(currProj.title)
+    $('.portfolio-modal .proj-desc').text(currProj.desc)
+    console.log(currProj);
+    
+}
---
+++ js/portfolio-services.js
@@ -0,0 +1,44 @@
+'use strict';
+var gProj;
+
+function creatProject() {
+	return [
+		{
+			id: 'sokoban',
+			name: 'Sokoban',
+			title: 'Better push those boxes',
+			desc: 'lorem ipsum lorem ipsum lorem ipsum',
+			url: 'projs/sokoban',
+			publishedAt: 1448693940000,
+			labels: ['Matrixes', 'keyboard events'],
+		},
+		{
+			id: 'bar',
+			name: 'bar',
+			title: 'Better push those boxes',
+			desc: 'lorem ipsum lorem ipsum lorem ipsum',
+			url: 'projs/sokoban',
+			publishedAt: 1448693940000,
+			labels: ['Matrixes', 'keyboard events'],
+		},
+		{
+			id: 'Minesweeper',
+			name: 'Minesweeper',
+			title: 'Better push those boxes',
+			desc: 'lorem ipsum lorem ipsum lorem ipsum',
+			url: 'projs/sokoban',
+			publishedAt: 1448693940000,
+			labels: ['Matrixes', 'keyboard events'],
+		},
+	];
+}
+
+function getProject() {
+	return (gProj = creatProject());
+}
+
+function getProjById(projId) {
+	return gProj.find(function (project) {
+		return project.id === projId;
+	});
+}
